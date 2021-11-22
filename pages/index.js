95739d092ac56071cd56e203567b2347e6ac5c19
luke-song <hyeonminsong7@gmail.com>
2021-11-22 14:58:52 -0800

    Making box component resoponsive

--- pages/index.js
+++ pages/index.js
@@ -15,27 +15,27 @@ export default function Home() {
 
         <Flex flexWrap="wrap" alignItems="center" justifyContent="center" maxW="800px" mt="10">
 
-          <Box as="a" href="http://localhost:3000/" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis="45%">
+          <Box as="a" href="http://localhost:3000/" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis={['auto', '45%']}>
             <Heading as="h3" size="lg" mb="2">Developer Page &rarr;</Heading>
               <Text fontSize="lg">Check out our Developer Page</Text>
           </Box>
 
-          <Box as="a" href="http://localhost:3000/" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis="45%">
+          <Box as="a" href="http://localhost:3000/" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis={['auto', '45%']}>
             <Heading as="h3" size="lg" mb="2">Style Adopter Page &rarr;</Heading>
               <Text fontSize="lg">Check out our Style Adopter Page</Text>
           </Box>
 
-        <Box as="a" href="http://localhost:3000/" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis="45%">
+        <Box as="a" href="http://localhost:3000/" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis={['auto', '45%']}>
         <Heading as="h3" size="lg" mb="2">Philter Page &rarr;</Heading>
         <Text fontSize="lg">Check out our Philter Page</Text>
         </Box>
 
-        <Box as="a" href="http://localhost:3000/" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis="45%">
+        <Box as="a" href="http://localhost:3000/" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis={['auto', '45%']}>
         <Heading as="h3" size="lg" mb="2">Image Restoration Page &rarr;</Heading>
         <Text fontSize="lg">Check out our Image Restoration Page</Text>
         </Box>
 
-        <Box as="a" href="http://localhost:3000/" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis="45%">
+        <Box as="a" href="http://localhost:3000/" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis={['auto', '45%']}>
         <Heading as="h3" size="lg" mb="2">Gallery Page &rarr;</Heading>
         <Text fontSize="lg">Check out our Gallery Page</Text>
         </Box>

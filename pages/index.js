8fed74e3408633477c27add259d92ae2b08c6214
luke-song <hyeonminsong7@gmail.com>
2021-12-03 00:43:37 -0800

    added sample images for homepage for later ImageURL implementation, added a new index for image to load filtered image when style image is clicked.

--- pages/index.js
+++ pages/index.js
@@ -25,28 +25,42 @@ export default function Home() {
   function ImagesContainer() {
     const images = [
       "https://picsum.photos/200?random=1", //IMAGE URL FROM SRI ON FRIDAY
-      "https://picsum.photos/200?random=2",
+      "https://picsum.photos/200?random=2", //
       "https://picsum.photos/200?random=3",
       "https://picsum.photos/200?random=4",
       "https://picsum.photos/200?random=5",
-      "https://picsum.photos/200?random=6", //When you press the image, make
+      "https://picsum.photos/200?random=6",
     ];
-    const [selectedImage, setSelectedImage] = useState(images[0]);
+
+    const filteredImages = [
+      "https://picsum.photos/200?random=7", //IMAGE URL FROM SRI ON FRIDAY
+      "https://picsum.photos/200?random=8", //
+      "https://picsum.photos/200?random=9",
+      "https://picsum.photos/200?random=10",
+      "https://picsum.photos/200?random=11",
+      "https://picsum.photos/200?random=12",
+    ];
+
+    const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+
     return (
       <div>
-        <SelectedImage url={selectedImage} />
-        {images.map((url) => (
+        <SelectedImage url={filteredImages[selectedImageIndex]} />
+        {images.map((url, i) => (
           <ImageGridItem
             url={url}
-            selected={url === selectedImage}
-            onClick={() => setSelectedImage(url)}
+            selected={url === images[selectedImageIndex]}
+            onClick={(e, { selectedImageIndex }) =>
+              this.setState({ activeIndex: index, isOpen: true })
+            }
           />
         ))}
       </div>
     );
   }
+
   function SelectedImage({ url }) {
-    return <img src={url} style={{ width: 250 }} />; // instead of selected image, you have to load another radom image
+    return <img src={url} style={{ width: 250 }} />; // instead of selected image, load another radom image for now
   }
 
   function ImageGridItem({ url, selected, onClick }) {
@@ -112,8 +126,26 @@ export default function Home() {
               minH="700"
             >
               <Container textAlign="center" textColor="RED" p={2}>
-                {" "}
-                Style Adopter feature is not yet ready
+                <div align="center">
+                  <Image
+                    src="https://picsum.photos/200?random=13"
+                    boxSize="300px"
+                  />
+                </div>
+                <div flexDirection="row">
+                  <Image
+                    margin="50px"
+                    display="inline-flex"
+                    src="https://picsum.photos/200?random=13"
+                    boxSize="150px"
+                  />
+                  <Image
+                    margin="50px"
+                    display="inline-flex"
+                    src="https://picsum.photos/200?random=15"
+                    boxSize="150px"
+                  />
+                </div>
               </Container>
             </SimpleGrid>
 
@@ -131,7 +163,26 @@ export default function Home() {
               backgroundColor="white"
               maxW="1000"
               minH="700"
-            ></SimpleGrid>
+            >
+              <Container textAlign="center" textColor="RED" p={2}>
+                <div flexDirection="row">
+                  <Image
+                    src="https://picsum.photos/200?random=15"
+                    width={200}
+                    height={500}
+                    display="inline-flex"
+                    margin="25px"
+                  />
+                  <Image
+                    margin="25px"
+                    display="inline-flex"
+                    src="https://picsum.photos/200?random=15"
+                    width={200}
+                    height={500}
+                  />
+                </div>
+              </Container>
+            </SimpleGrid>
 
             <Box align="center" my={4}>
               <NextLink href="/restoration">

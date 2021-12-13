106b61bdc77b15e10e30e34294af5ce5232de67d
PatrickLChau <82122383+PatrickLChau@users.noreply.github.com>
2021-12-13 00:26:00 -0800

    Added comments

--- components/Modal.js
+++ components/Modal.js
@@ -1,33 +1,50 @@
-// Pop-up Component for Gallery page
-
+/**
+ * The Modal component to for use within GalleryImage, it is the GalleryImage's child.
+ * Generates a popup on clicking the Gallery Image and calls
+ * an API to enable deletion of images from the gallery.
+ * Utilizes Axios, React, NextJS, and Chakra-UI.
+ */
 import React, { useState } from "react";
 import { Image } from "@chakra-ui/image";
 import { SimpleGrid, Box, Input } from "@chakra-ui/layout";
 import { CloseButton } from "@chakra-ui/close-button";
 import { Button } from "@chakra-ui/button";
-import axios from "axios";
+import axios from 'axios'
 import { color } from "@chakra-ui/react";
 
+//The popup
 function Popup(props) {
-  const id = props.id;
+  /**
+   * Used to keep track of the id for deleting images
+   */
+  const id = props.id
 
-  async function dbData(id) {
+/**
+ * The API call to the database to delete an image from the Gallery
+ * @param {String} id The string used to delete image and associated images from gallery
+ * @returns nothing
+ */
+  async function dbData (id) {
     console.log("trying to delete data");
     const form = new FormData();
     form.append("clusterId", id);
     return axios({
-      url: "http://127.0.0.1:5000/delete-cluster",
-      data: form,
-      method: "post",
-      headers: { "Content-Type": "multipart/form-data" },
-      mode: "cors",
-    });
+        url: "http://127.0.0.1:5000/delete-cluster",
+        data: form,
+        method: "post",
+        headers: { "Content-Type": "multipart/form-data" },
+        mode: "cors",
+      });
   }
 
-  return props.trigger ? (
-    <div className="popup">
-      <style jsx>
-        {`
+
+    /**
+     * Uses a ternary operator with props.trigger as a boolean to have the popup be enabled or disabled
+     */
+    return (props.trigger) ? (
+        <div className="popup">
+          {/**The in-code css for the various parts */}
+        <style jsx>{`
         .popup {
             position: fixed;
       
@@ -78,99 +95,53 @@ function Popup(props) {
           }
           
         `}
-      </style>
-      <div className="popup-inner">
+        </style>
+        <div className="popup-inner">
         {/* <button className="close-btn" onClick={()=>props.setTrigger(false)}>
             x
             </button> */}
-        <SimpleGrid columns="2" bgColor="">
-          <Box backgroundColor="" textAlign="left" bgColor="white">
-            Old
-            {
-              <Image
-                src={props.baseSrc[0].url}
-                boxSize={245}
-                objectFit="cover"
-                borderRadius={30}
-                marginTop={61}
-              />
-            }
-            {props.algo == "BW" && (
-              <Image
-                src={props.baseSrc[0].url}
-                boxSize={245}
-                objectFit="cover"
-                borderRadius={30}
-                marginTop={61}
-              />
-            )}
-            {props.algo != "BW" && (
-              <Image
-                src={props.baseSrc[1].url}
-                boxSize={245}
-                objectFit="cover"
-                borderRadius={30}
-                marginTop={61}
-              />
-            )}
-          </Box>
-          <Box textColor="black" textAlign="left" fontSize={19}>
-            <CloseButton
-              boxSize="50"
-              onClick={() => props.setTrigger(false)}
-              color="red"
-              bgColor="#ebedf0"
-              _hover={{ bg: "rgb(255, 128, 128)" }}
-              marginLeft={385}
-              borderRadius={15}
-            ></CloseButton>
-            <Box textAlign="left">
-              New
-              <Image
-                src={props.src}
-                boxSize={325}
-                objectFit="cover"
-                borderRadius={30}
-                marginTop={31}
-              />
+            {/**A Simple Grid structure of 2 columns to split the popup into 2. Places 2 source images into first and 1 new images and data into second */}
+            <SimpleGrid columns="2" bgColor="">
+              {/**First column containing 2 source images. Posts the first image twice if given algorithm ID is BW*/}
+              <Box backgroundColor='' textAlign='left' bgColor='white'>
+                Old
+                {/**First Image */}
+              {<Image src={props.baseSrc[0].url} boxSize={245} objectFit="cover" borderRadius={30} marginTop={61}/>}
+              {/**Second image is the first image but repeated if algorithm ID is BW */}
+              {(props.algo == 'BW') && <Image src={props.baseSrc[0].url} boxSize={245} objectFit="cover" borderRadius={30} marginTop={61}/>}
+              {/**Second source image if algorithm ID is not BW */}
+              {(props.algo != 'BW') && <Image src={props.baseSrc[1].url} boxSize={245} objectFit="cover" borderRadius={30} marginTop={61}/>}
+              </Box>
+              {/**Second Column */}
+            <Box textColor="black" textAlign="left" fontSize={19}>
+              {/**Close button that disables the model by sending a setTrigger prop of false  to GalleryImage*/}
+              <CloseButton boxSize="50" onClick={()=>props.setTrigger(false)} color="red" bgColor="#ebedf0" _hover={{ bg: 'rgb(255, 128, 128)' }} marginLeft={385} borderRadius={15}>
+                </CloseButton>
+                <Box textAlign='left'>
+                New
+                {/**Image on the right showing new, stylized image */}
+              <Image src={props.src} boxSize={325} objectFit="cover" borderRadius={30} marginTop={31}/>
+              </Box>
             </Box>
-          </Box>
-        </SimpleGrid>
-        <Box
-          bgColor=""
-          boxSize="250"
-          minWidth="400"
-          textAlign="left"
-          textColor="black"
-          marginTop={-200}
-          marginLeft={440}
-        >
-          Tags: <br />
-          {props.tags} <br />
-          <br />
-          Name: <br />
-          {props.name} <br />
-          Description: <br />
-          {props.description} <br />
-          <Button
-            bgColor="rgb(255, 102, 102)"
-            marginTop={8}
-            marginLeft={345}
-            _hover={{ bgColor: "rgb(230, 0, 0)" }}
-            onClick={() => {
-              dbData(id).then((response) => console.log(response));
-              props.setSrc(false);
-            }}
-          >
-            Delete
-          </Button>
-        </Box>
-        {props.children}
-      </div>
-    </div>
-  ) : (
-    ""
-  );
+            </SimpleGrid>
+            {/**Still in second column, DIsplaying gathered relevant data of the images in the Modal */}
+            <Box bgColor="" boxSize="250" minWidth="400" textAlign="left" textColor="black" marginTop={-200} marginLeft={440}>
+            Tags: <br/>
+              {props.tags} <br/><br/>
+              Name: <br/>
+              {props.name} <br/>
+              Description: <br/>
+              {props.description} <br/>
+              {/**Delete button that calls the API to delete in the database and disables the GalleryImage by sending a setSrc prop of false */}
+              <Button bgColor='rgb(255, 102, 102)' marginTop={8} marginLeft={345} _hover={{bgColor: 'rgb(230, 0, 0)'}} onClick={() => {(dbData(id).then(response => console.log(response))); props.setSrc(false)}}>
+                Delete
+              </Button>
+              </Box>
+            {props.children}
+          </div>
+        </div>
+
+    ) : "";
 }
 
-export default Popup;
+export default Popup
